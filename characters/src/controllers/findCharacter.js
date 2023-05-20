394a640655cea4a189f89e948cc2aa9ef31df344
Cristian Murua <cristianmurua1995@gmail.com>
2023-05-20 00:01:34 -0300

    find route fix

--- characters/src/controllers/findCharacter.js
+++ characters/src/controllers/findCharacter.js
@@ -2,6 +2,7 @@ const Character = require("../data");
 const { response } = require("../utils");
 
 module.exports = async (req, res) => {
-  const character = await Character.find();
+  const { id } = req.params;
+  const character = await Character.find(id);
   response(res, 200, character);
 };
